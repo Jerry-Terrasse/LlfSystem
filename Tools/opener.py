1307568d25f21b10b5782b7e90dd033eb11a3851
Terrasse <3305049949@qq.com>
2019-05-14 18:54:58 +0800

    Fight more regularly

---
+++ Tools/opener.py
@@ -0,0 +1,27 @@
+import os
+import time
+
+# (80) (220,835) (360) (500)
+# (290,666)
+
+
+
+def adb_click(x,y):
+  os.system("adb_server shell input tap "+str(x)+" "+str(y))
+
+
+if __name__=='__main__':
+  print("Opener Here")
+  time.sleep(3600*3)
+  adb_click(220,835)
+  time.sleep(20)
+  adb_click(290,666)
+  time.sleep(3600*3)
+  adb_click(500,835)
+  time.sleep(20)
+  adb_click(290,666)
+  time.sleep(3600*3)
+  adb_click(80,835)
+  time.sleep(20)
+  adb_click(290,666)
+  os.system("notepad")
---
+++ debugger.py
@@ -0,0 +1,18 @@
+from PIL import ImageGrab
+import time
+import cv2
+import numpy as np
+
+def tk():
+  for i in range(11):
+    ImageGrab.grab((640,680,695,700)).save(str(i)+".png")
+    time.sleep(3)
+    
+
+def showimg(str):
+  img=cv2.imread(str,0)
+  ret,img=cv2.threshold(img,0,255,cv2.THRESH_BINARY|cv2.THRESH_OTSU)
+  cv2.namedWindow('1',cv2.WINDOW_AUTOSIZE)
+  cv2.imshow('1',img)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
