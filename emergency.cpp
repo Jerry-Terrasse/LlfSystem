2ac2c8517b7b23e95c1c58d35632aaef876b443c
Terrasse <3305049949@qq.com>
2019-05-18 12:32:20 +0800

    remove useless part

--- emergency.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include "unistd.h"
-using namespace std;
-int main()
-{
-  for(;;sleep(400),system("taskkill /f /im python.exe"));
-  //for(;;sleep(4),system("taskkill /f /im python.exe"));
-  return 0;
-}
